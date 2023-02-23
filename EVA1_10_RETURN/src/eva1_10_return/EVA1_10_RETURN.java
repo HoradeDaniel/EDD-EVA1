4b0180ac185d41b766721c4979a647b83d35092c
HoradeDaniel <124392783+HoradeDaniel@users.noreply.github.com>
2023-02-23 00:55:37 -0600

    Add files via upload

---
+++ EVA1_10_RETURN/src/eva1_10_return/EVA1_10_RETURN.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_10_return;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_10_RETURN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      square(10);//llamada a función, pero sin procesar el resultado
+      resu = square(3);
+      System.out.println(resu);
+    }
+    public static int aquare (int num){
+        return num * num;
+    }
+}
